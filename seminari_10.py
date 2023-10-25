65207d57bbf9e9dca01bd8c74b5d808b48d9bb7b
GiorgiTarsaidze <117092635+GiorgiTarsaidze@users.noreply.github.com>
2023-10-25 18:42:26 +0400

    Create seminari_10.py

---
+++ seminari_10.py
@@ -0,0 +1,39 @@
+# 1. In a file named "palindrome.py," create a program that prompts the
+# user for a word or phrase and checks if it is a palindrome.
+# A palindrome is a word, phrase, number, or other sequence of characters
+# that reads the same forward and backward (ignoring spaces, punctuation, and letter casing).
+# Output "True" if it's a palindrome, and "False" if it's not.
+
+  # # word = "bitcamp"
+    # # range(7,-1,-1)
+    # for i in range(len(word)-1,-1,-1):
+    #     new_word.append(word[i])
+    # print("".join(new_word))
+    # "note"
+    # print(new_word)
+
+
+# def main():
+#     word = input("Check if your phrase is palindrome: ")
+#     if isPalindrome(word):
+#         print("Your word is PALINDROME! ")
+#     else:
+#         print("This word is not palindrome! ")
+
+# def isPalindrome(word):
+#     reversed_word = word[::-1]
+#     if reversed_word == word:
+#         return True
+#     else:
+#         return False
+#     # დააბრუნოს ან True ან False
+
+# main()
+
+# my_list = [1,2,3,4,5,6,7]
+
+# print(my_list[::-1])
+
+
+# for i in reversed(range(4)):
+#     print(i)
