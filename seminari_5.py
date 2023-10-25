476bc6a1a74043bda53793f76340e67c93dd308f
GiorgiTarsaidze <117092635+GiorgiTarsaidze@users.noreply.github.com>
2023-10-25 18:41:09 +0400

    Create seminari_5.py

---
+++ seminari_5.py
@@ -0,0 +1,41 @@
+# 1. In a file named "palindrome.py," create a program that prompts the
+# user for a word or phrase and checks if it is a palindrome.
+# A palindrome is a word, phrase, number, or other sequence of characters
+# that reads the same forward and backward (ignoring spaces, punctuation, and letter casing).
+# Output "True" if it's a palindrome, and "False" if it's not.
+
+# 2. In a file named "calculator.py," implement a basic calculator program that allows
+# the user to perform addition, subtraction, multiplication, and division on two numbers.
+# Prompt the user for the operation they want to perform, and then for the two numbers
+# to operate on. Output the result of the operation.
+
+# 3. In a file named "guess_the_number.py," implement a simple number guessing game.
+# Generate a random number between 1 and 100, and have the user guess the number.
+# Provide hints like "Too high" or "Too low" until they guess the correct number.
+# Output the number of attempts it took to guess correctly.
+
+
+# name = "bitcamp"
+# guess = ""
+# counter = []
+
+# while True:
+#     # counter.append(guess)
+#     # print("You did not guess the name!")
+#     guess = input("Please guess the name: ").lower()
+#     if guess != name:
+#         print("Wrong name!")
+#         counter.append(guess)
+#     else:
+#         print("You guessed the name!")
+#         break
+
+#     # counter += 1
+# print(len(counter))
+
+
+
+# for i in range(20):
+#     if i == 15:
+#         break
+#     print(i)
