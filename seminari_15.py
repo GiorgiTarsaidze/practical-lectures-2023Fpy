08ac2f4a7c89e7498d81da543a4b1e64f4fee62d
GiorgiTarsaidze <117092635+GiorgiTarsaidze@users.noreply.github.com>
2023-11-04 11:20:56 +0400

    Update seminari_15.py

--- seminari_15.py
+++ seminari_15.py
@@ -103,3 +103,132 @@ if __name__ == "__main__":
 # ragaca_stringi = "wowwo wwow"
 
 # print(ragaca_stringi.split(" "))
+
+
+
+
+
+
+
+# ახალი ვერსია ----------------->
+
+# books = {
+#     "Topuria": ["Math", "fsf"],
+#     "Franz Kafka": "Gardasaxva",
+# }
+
+# def main():
+#     while True:
+#         print("1. View books")
+#         print("2. Add book")
+#         print("3. Remove book")
+#         print("4. Search book")
+#         print("5. Quit")
+
+#         choice = input("Enter your choice: ")
+
+#         if choice == "1":
+#             for element in view_books():
+#                 print(element)
+#         elif choice == "2":
+#             author = input("Enter new author: ").strip()
+#             book = input("Enter new book: ").strip()
+
+#             if add_book(author,book):
+#                 print("Book added!")
+#         elif choice == "3":
+#             author = input("Enter author your want to remove: ").strip()
+#             book = input("book: ").strip()
+#             if remove_book(author,book):
+#                 print("Book successfully removed!")
+#             else:
+#                 print("Book not found in library.")
+#         elif choice == "4":
+#             book = input("Enter book: ").strip()
+#             if search_book(book):
+#                 print("Book Available in our library!")
+#             else:
+#                 print("Book not found!")
+#         elif choice == "5":
+#             print("Exiting program.")
+#             break
+#         else:
+#             print("Invalid choice.")
+
+
+
+# def view_books():
+#     sorted_books = []
+#     for key, value in books.items():
+#         if isinstance(value, list):
+#             sorted_books.append(f"{key} - {', '.join(value)}")
+#         else:
+#             sorted_books.append(f"{key} - {value}")
+#     return sorted_books
+
+
+
+
+# def add_book(author,book):
+#     # 1
+#     # if author in books and isinstance(books[author], list):
+#     #     books[author].append(book)
+#     # elif author in books and isinstance(books[author], str):
+#     #     books[author] = books[author].split()
+#     #     books[author].append(book)
+#     #     # books[author] = book # list
+#     # else:
+#     #     books[author] = book
+#     # print(books)
+
+#     # 2
+#     try:
+#         books[author].append(book)
+#     except KeyError:
+#         books[author] = book
+#     except AttributeError:
+#         simple_list = []
+#         simple_list.append(books[author])
+#         simple_list.append(book)
+#         books[author] = simple_list
+
+#     return True
+
+# # def remove_book(author):
+# #     if author in books:
+# #         books.pop(author)
+# #         return True
+# #     return False
+#     # კონკრეტული წიგნი წასაშლელი
+
+# books = {
+#     "Topuria": ["Math", "fsf"],
+#     "Franz Kafka": "Gardasaxva",
+# }
+
+# def remove_book(author, book):
+#     if author in books:
+#         if isinstance(books[author], list):
+#             if book in books[author]:
+#                 books[author].remove(book)
+#                 return True
+#         elif book == books[author]:
+#             books.pop(author)
+#             return True
+#     return False
+
+# def search_book(book):
+#     # dict_values = books.values()
+
+#     # ჩასამატებელი მაგალთად 'ga' -ზე მაინც True-ს აბრუნებს
+#     for element in books:
+#         if book in books[element]:
+#             return True
+#     return False
+
+# if __name__ == "__main__":
+#     main()
+
+
+
+
